4117bdf0c1251b386698a2d54a23b81c414e971e
Wang-yijun <yijun0509wang@gmail.com>
2022-04-26 23:07:10 -0700

    test: add things back

--- source/particle/property/stress.cc
+++ source/particle/property/stress.cc
@@ -42,9 +42,10 @@ namespace aspect
       template <int dim>
       void
       Stress<dim>::update_particle_property(const unsigned int data_position,
-                                                      const Vector<double> &/*solution*/,
-                                                      const std::vector<Tensor<1,dim> > &gradients,
-                                                      typename ParticleHandler<dim>::particle_iterator &particle) const
+                                            const Point<dim> &position,
+                                            const Vector<double> &solution,
+                                            const std::vector<Tensor<1,dim> > &gradients,
+                                            typename ParticleHandler<dim>::particle_iterator &particle) const
       {
         auto &data = particle->get_properties();
         // Velocity gradients
@@ -54,11 +55,44 @@ namespace aspect
         //std::cout<<"grad_u: "<<grad_u<<std::endl;
         // Calculate strain rate from velocity gradients
         const SymmetricTensor<2,dim> strain_rate = symmetrize (grad_u);
-        //std::cout<<"Stress: "<<strain_rate<<std::endl;
+        //std::cout<<"strainrate: "<<strain_rate<<std::endl;
+        const SymmetricTensor<2,dim> deviatoric_strain_rate
+              = (this->get_material_model().is_compressible()
+                 ?
+                 strain_rate - 1./3 * trace(strain_rate) * unit_symmetric_tensor<dim>()
+                 :
+                 strain_rate);
 
+
+        double pressure = solution[this->introspection().component_indices.pressure];
+        double temperature = solution[this->introspection().component_indices.temperature];
+        Tensor<1,dim> velocity;
+        for (unsigned int i = 0; i < dim; ++i)
+            velocity[i] = solution[this->introspection().component_indices.velocities[i]];
+        std::vector<double> compositions;
+        for (unsigned int i = 0; i < this->n_compositional_fields(); i++)
+            {
+                const unsigned int solution_component = this->introspection().component_indices.compositional_fields[i];
+                compositions.push_back(solution[solution_component]);
+            }
+
+        MaterialModel::MaterialModelInputs<dim> material_model_inputs(1,this->n_compositional_fields());
+        material_model_inputs.position[0] = position;
+        material_model_inputs.temperature[0] = temperature;
+        material_model_inputs.pressure[0] = pressure;
+        material_model_inputs.velocity[0] = velocity;
+        material_model_inputs.composition[0] = compositions;
+        material_model_inputs.strain_rate[0] = strain_rate;
+
+        MaterialModel::MaterialModelOutputs<dim> material_model_outputs(1,this->n_compositional_fields());
+        this->get_material_model().evaluate(material_model_inputs, material_model_outputs);
+        double eta = material_model_outputs.viscosities[0];
+
+        const SymmetricTensor<2,dim> stress = 2 * eta * deviatoric_strain_rate +
+                                            pressure * unit_symmetric_tensor<dim>();
 
         for (unsigned int i = 0; i < Tensor<2,dim>::n_independent_components ; ++i) 
-          data[data_position + i] = strain_rate[Tensor<2,dim>::unrolled_to_component_indices(i)];
+          data[data_position + i] = stress[Tensor<2,dim>::unrolled_to_component_indices(i)];
         
        
       }
@@ -97,9 +131,9 @@ namespace aspect
     namespace Property
     {
       ASPECT_REGISTER_PARTICLE_PROPERTY(Stress,
-                                        "Stress",
+                                        "stress",
                                         "Implementation of a plugin in which the particle "
-                                        "property is defined as the recent Stress at "
+                                        "property is defined as the recent stress at "
                                         "this position.")
     }
   }
