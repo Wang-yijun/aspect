0ffd8497401f8ad76c6b5479ae9db02e9ad6504a
Wang-yijun <yijun0509wang@gmail.com>
2023-02-18 17:20:19 +0100

    Add initialize and elastic stress to stress property for test - fixed signs

--- source/particle/property/stress.cc
+++ source/particle/property/stress.cc
@@ -89,15 +89,15 @@ namespace aspect
         // Add elastic stresses if existent
         if (this->get_parameters().enable_elasticity == true)
             {
-                stress[0][0] += material_model_inputs.composition[0][this->introspection().compositional_index_for_name("ve_stress_xx")];
-                stress[1][1] += material_model_inputs.composition[0][this->introspection().compositional_index_for_name("ve_stress_yy")];
-                stress[0][1] += material_model_inputs.composition[0][this->introspection().compositional_index_for_name("ve_stress_xy")];
+                stress[0][0] = stress[0][0] + material_model_inputs.composition[0][this->introspection().compositional_index_for_name("ve_stress_xx")];
+                stress[1][1] = stress[1][1] + material_model_inputs.composition[0][this->introspection().compositional_index_for_name("ve_stress_yy")];
+                stress[0][1] = stress[0][1] + material_model_inputs.composition[0][this->introspection().compositional_index_for_name("ve_stress_xy")];
 
                 if (dim == 3)
                     {
-                        stress[2][2] += material_model_inputs.composition[0][this->introspection().compositional_index_for_name("ve_stress_zz")];
-                        stress[0][2] += material_model_inputs.composition[0][this->introspection().compositional_index_for_name("ve_stress_xz")];
-                        stress[1][2] += material_model_inputs.composition[0][this->introspection().compositional_index_for_name("ve_stress_yz")];
+                        stress[2][2] = stress[2][2] + material_model_inputs.composition[0][this->introspection().compositional_index_for_name("ve_stress_zz")];
+                        stress[0][2] = stress[0][2] + material_model_inputs.composition[0][this->introspection().compositional_index_for_name("ve_stress_xz")];
+                        stress[1][2] = stress[1][2] + material_model_inputs.composition[0][this->introspection().compositional_index_for_name("ve_stress_yz")];
                     }
             }
 
